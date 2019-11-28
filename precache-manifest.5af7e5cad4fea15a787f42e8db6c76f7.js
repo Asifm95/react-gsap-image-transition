a29f7c906f6837e97ace4a27d1fbe9f5e99fcca3
Asif Mohammed <asifm2882@gmail.com>
2019-11-28 16:46:58 +0530

    Updates

---
+++ precache-manifest.5af7e5cad4fea15a787f42e8db6c76f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486f923de4492d9e17c1448ca98d476a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4a9386c524c61d9411",
+    "url": "/static/css/main.6cfb06a7.chunk.css"
+  },
+  {
+    "revision": "c3e1d8db6231a3be103d",
+    "url": "/static/js/2.d5b62f65.chunk.js"
+  },
+  {
+    "revision": "fb4a9386c524c61d9411",
+    "url": "/static/js/main.aecde724.chunk.js"
+  },
+  {
+    "revision": "aebe50c19beb70f2a2de",
+    "url": "/static/js/runtime-main.5b9c14b7.js"
+  },
+  {
+    "revision": "4152ca00cecedda36761211e967fdd93",
+    "url": "/static/media/darthvader.4152ca00.jpg"
+  }
+]);
