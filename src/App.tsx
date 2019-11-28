d4ced4551a1f2ee4bf1d92f90d9a8fdd7ee751b6
Asif Mohammed <asifm2882@gmail.com>
2019-11-28 15:39:09 +0530

    add image animation :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,26 @@ import gsap from 'gsap';
 import './App.scss';
 
 const App: React.FC = () => {
+  let image = useRef<HTMLImageElement | null>(null);
+  let container = useRef<HTMLDivElement | null>(null);
+  let imgReveal = useRef<HTMLDivElement | null>(null);
+  let tl = gsap.timeline();
+  useEffect(() => {
+    if (container.current && image.current && imgReveal.current) {
+      tl.to(container.current, { duration: 0, css: { visibility: 'visible' } })
+        .to(imgReveal.current, {
+          duration: 1.3,
+          width: '0%',
+          ease: 'Power2.easeInOut'
+        })
+        .from(image.current, {
+          duration: 1.2,
+          scale: 1.6,
+          ease: 'Power2.easeIn',
+          delay: -1.3
+        });
+    }
+  });
   return (
     <section className="main">
       <p>WELCOME TO THE DARK SIDE</p>
